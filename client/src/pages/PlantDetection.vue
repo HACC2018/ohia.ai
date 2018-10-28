4b79c79f6acbf21ec76e44d42fc3c367d91a99b8
spjy <spjy@hawaii.edu>
2018-10-28 01:19:29 -1000

    feat: Added detector page with camera code, added respective route

---
+++ client/src/pages/PlantDetection.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn color="primary" label="Get Picture" @click="captureImage" />
+    <img :src="imageSrc">
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageSrc: '',
+    };
+  },
+  methods: {
+    captureImage() {
+      navigator.camera.getPicture(
+        (data) => { // on success
+          this.imageSrc = `data:image/jpeg;base64,${data}`;
+        },
+        () => { // on fail
+          this.$q.notify('Could not access device camera.');
+        },
+        {
+          quality: 75,
+        },
+      );
+    },
+  },
+};
+</script>
