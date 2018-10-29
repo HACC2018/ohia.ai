cdcc923a2cf5e003d54b997a73eb31c1bed80700
Unknown <mhmotoki@gmail.com>
2018-10-29 09:13:53 -1000

    write regex to clean up plant categories

--- data_collection/download_raw_images.R
+++ data_collection/download_raw_images.R
@@ -1,20 +1,22 @@
-#scraper for http://www.starrenvironmental.com/images/
-#create a table of species names, common names, etc., and scrape pictures with associated species
 library(rvest)
 
-download_html <- function(url) {
+download_html <- function(url, os='windows') {
   
   # create phantomjs script
-  x <- readLines('template.js')
+  x <- readLines('js/thumbnail_template.js')
   x <- sub('REPLACEMENT_URL', sprintf("'%s'", url), x)
-  fc <- file('get_site.js')
+  fc <- file('js/download.js')
   writeLines(x, fc)
   close(fc)
   
   # run phantomjs
-  system('phantomjs.exe get_site.js')
+  if (os == 'windows') {
+    system('phantomjs.exe js/download.js')
+  } else {
+    system('phantomjs js/download.js')
+  }
 }
-
+ 
 # get list of species
 base_url <- "http://www.starrenvironmental.com/images"
 species_list <- read_html(base_url) %>%
@@ -22,13 +24,14 @@ species_list <- read_html(base_url) %>%
   html_text()
 head(species_list)
 
+
 # download all images
 for (species in species_list) {
   
   # download html
   species_url <- gsub(' ', '+', species)
   species_url <- sprintf('%s/search/?q=%s', base_url, species_url)
-  download_html(species_url)
+  download_html(species_url, os='ubuntu')
   
   # parse html
   image_url_list <- read_html('species.html') %>%
