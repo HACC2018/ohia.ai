bc305132fa758d6e04c7bee6e0c30f597654588c
Marifel Barbasa <mrbarbasa@gmail.com>
2018-10-29 23:31:04 -1000

    Add CORS middleware required for browser POST requests

--- server/index.js
+++ server/index.js
@@ -19,6 +19,12 @@ function initializeServices() {
 
 // Middleware
 app.use(express.json());
+// Required for browser POST requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello ohia.ai');
