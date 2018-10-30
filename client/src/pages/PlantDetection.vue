31cd440fc616f1235133ea845181811e55bf9a0b
Marifel Barbasa <mrbarbasa@gmail.com>
2018-10-29 23:24:12 -1000

    Implement a working POST request

--- client/src/pages/PlantDetection.vue
+++ client/src/pages/PlantDetection.vue
@@ -8,6 +8,7 @@
       <q-btn color="primary" label="Detect Plant" @click="captureImage" />
 
       <img :src="imageSrc">
+      {{ testing }}
     </q-card-main>
   </q-card>
 </template>
@@ -20,34 +21,34 @@ export default {
   name: 'PlantDetection',
   data() {
     return {
+      testing: 'start',
       imageSrc: '',
     };
   },
   methods: {
     captureImage() {
-      const { camera } = navigator;
-      camera.getPicture(
-        (data) => { // on success
-          this.imageSrc = `data:image/jpeg;base64,${data}`;
-        },
-        () => { // on fail
-          this.$q.notify('Could not access device camera.');
-        },
-        {
-          // Camera options
-          // Refer to:
-          //   https://cordova.apache.org/docs/en/latest/reference/
-          //   cordova-plugin-camera/index.html#cameracameraoptions--object
-          quality: 75, // 75% of full image quality
-          destinationType: camera.DestinationType.DATA_URL, // Return native uri for iOS
-          sourceType: camera.PictureSourceType.CAMERA, // Use camera
-          allowEdit: false, // Allow editing of image before selection
-          encodingType: camera.EncodingType.JPEG, // Or PNG
-          correctOrientation: true, // Allow for correction if device is rotated
-          saveToPhotoAlbum: false, // Do not save the image to user Photos
-          cameraDirection: camera.Direction.BACK, // Use the back-facing camera
-        },
-      );
+      // const imageData = new FormData();
+      const payload = {
+        // uri: filePath,
+        // name: 'test_photo.jpg',
+        // type: 'image/jpg',
+        sample: 'payload',
+      };
+      // imageData.append('json', JSON.stringify(payload));
+
+      const url = 'https://big-vampirebat-16.localtunnel.me/images/upload';
+      this.$axios.post(url, payload)
+        .then((res) => {
+          this.testing = res.data.success;
+        })
+        .catch(() => {
+          this.$q.notify({
+            color: 'negative',
+            position: 'top',
+            message: 'Image upload failed',
+            icon: 'report_problem',
+          });
+        });
     },
   },
 };
