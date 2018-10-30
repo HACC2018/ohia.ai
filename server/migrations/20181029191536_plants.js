9934e7a1e5ee50cf5a7e9831d2601ca362d42b1e
Spencer <spjy@hawaii.edu>
2018-10-29 19:36:29 -1000

    fix: Added needed rows for plants

--- server/migrations/20181029191536_plants.js
+++ server/migrations/20181029191536_plants.js
@@ -2,10 +2,14 @@ exports.up = (knex, Promise) => {
   knex.schema.createTable('plants', (table) => {
     table.increments('id').primary();
 
+    table.string('plant_name');
+    table.string('status');
+    table.string('description');
     table.string('genus');
     table.string('species');
     table.string('common_name');
     table.string('hawaiian_name');
+    table.string('scientific_name');
     table.text('story');
     table.text('use');
     table.enu('endangered',
