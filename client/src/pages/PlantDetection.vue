ad92899ba1bda7617ce75876d507c608daab80f9
Marifel Barbasa <mrbarbasa@gmail.com>
2018-10-31 04:32:04 -1000

    Re-enable eslint and clean up some code on the PlantDetection page

--- client/src/pages/PlantDetection.vue
+++ client/src/pages/PlantDetection.vue
@@ -29,66 +29,84 @@ export default {
   },
   methods: {
     captureImage() {
+      const view = this;
       const { camera } = navigator;
+      const cameraOptions = {
+        // Camera options
+        // Refer to:
+        //   https://cordova.apache.org/docs/en/latest/reference/
+        //   cordova-plugin-camera/index.html#cameracameraoptions--object
+        quality: 50, // 50% of full image quality
+        destinationType: camera.DestinationType.NATIVE_URI, // Return native uri for iOS
+        sourceType: camera.PictureSourceType.CAMERA, // Use camera
+        allowEdit: false, // Allow editing of image before selection
+        encodingType: camera.EncodingType.JPEG, // Or PNG
+        correctOrientation: true, // Allow for correction if device is rotated
+        saveToPhotoAlbum: false, // Do not save the image to user Photos
+        cameraDirection: camera.Direction.BACK, // Use the back-facing camera
+      };
+      const displayErrorMessage = () => {
+        view.$q.notify({
+          color: 'negative',
+          position: 'top',
+          message: 'Image upload failed',
+          icon: 'report_problem',
+        });
+      };
+
       camera.getPicture(
         (filePath) => {
-          const view = this;
-            window.resolveLocalFileSystemURL(filePath, function(fileEntry) {
-              fileEntry.file(function(file) {
-                const reader = new FileReader();
-                reader.onloadend = function(event) {
-                  console.log('Image byte length:', event.target.result.byteLength);
-                  // Create a blob based on the FileReader "result",
-                  // which we asked to be retrieved as an ArrayBuffer
-                  const arrayBufferView = new Uint8Array(this.result);
-                  const blob = new Blob([arrayBufferView], { type: 'image/jpeg' });
-                  const formData = new FormData();
-                  formData.append('latitude', view.latitude);
-                  formData.append('longitude', view.longitude);
-                  formData.append('image', blob);
-
-                  const appHost = 'https://8e7ef90d.ngrok.io';
-                  const imageUploadUrl = `${appHost}/images/upload`;
-                  view.$axios.post(imageUploadUrl, formData)
-                    .then((res) => {
-                      view.testing = res.data.success;
-                    })
-                    .catch(() => {
-                      view.$q.notify({
-                        color: 'negative',
-                        position: 'top',
-                        message: 'Image upload failed',
-                        icon: 'report_problem',
-                      });
-                    });
-                };
-                console.log('Reading file:', file.name);
-                // Read the file as an ArrayBuffer
-                reader.readAsArrayBuffer(file);
-              }, function(err) {
-                console.error('Error getting fileEntry file:', err);
+          const convertImageToBlob = (result) => {
+            // Create a blob based on the FileReader "result",
+            // which we asked to be retrieved as an ArrayBuffer
+            const arrayBufferView = new Uint8Array(result);
+            return new Blob([arrayBufferView], { type: 'image/jpeg' });
+          };
+          const constructFormData = (blob) => {
+            const formData = new FormData();
+            formData.append('latitude', view.latitude);
+            formData.append('longitude', view.longitude);
+            formData.append('image', blob);
+            return formData;
+          };
+          function uploadImage() {
+            const blob = convertImageToBlob(this.result);
+            const formData = constructFormData(blob);
+            const appHost = 'https://8e7ef90d.ngrok.io';
+            const imageUploadUrl = `${appHost}/images/upload`;
+            view.$axios.post(imageUploadUrl, formData)
+              .then((res) => {
+                view.testing = res.data.success;
+              })
+              .catch(() => {
+                displayErrorMessage();
               });
-            }, function(err) {
-              console.error('Error resolving the local file system URL:', err);
+          }
+          window.resolveLocalFileSystemURL(filePath, (fileEntry) => {
+            fileEntry.file((file) => {
+              const reader = new FileReader();
+              reader.onloadend = uploadImage;
+              // Read the file as an ArrayBuffer
+              reader.readAsArrayBuffer(file);
+            }, (err) => {
+              console.error('Error retrieving the fileEntry file:', err);
+              displayErrorMessage();
             });
+          }, (err) => {
+            console.error('Error resolving the local file system URL:', err);
+            displayErrorMessage();
+          });
         },
-        () => {
-          this.$q.notify('Could not access device camera.');
+        (err) => {
+          console.error('Error accessing the device camera:', err);
+          view.$q.notify({
+            color: 'negative',
+            position: 'top',
+            message: 'Could not access device camera',
+            icon: 'report_problem',
+          });
         },
-        {
-          // Camera options
-          // Refer to:
-          //   https://cordova.apache.org/docs/en/latest/reference/
-          //   cordova-plugin-camera/index.html#cameracameraoptions--object
-          quality: 50, // 50% of full image quality
-          destinationType: camera.DestinationType.NATIVE_URI, // Return native uri for iOS
-          sourceType: camera.PictureSourceType.CAMERA, // Use camera
-          allowEdit: false, // Allow editing of image before selection
-          encodingType: camera.EncodingType.JPEG, // Or PNG
-          correctOrientation: true, // Allow for correction if device is rotated
-          saveToPhotoAlbum: false, // Do not save the image to user Photos
-          cameraDirection: camera.Direction.BACK, // Use the back-facing camera
-        },
+        cameraOptions,
       );
     },
   },
