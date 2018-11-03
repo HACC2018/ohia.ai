dc36dcf43d7598c49f5f9a37f779210987149af0
Marifel Barbasa <mrbarbasa@gmail.com>
2018-11-02 22:09:17 -1000

    Make the `plant_name` column in the `plants` table unique

--- server/migrations/20181029191536_plants.js
+++ server/migrations/20181029191536_plants.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('plants', (table) => {
     table.increments('id').primary();
 
-    table.string('plant_name').notNullable();
+    table.string('plant_name').notNullable().unique();
     table.enu('status',
     [
       'Native',
