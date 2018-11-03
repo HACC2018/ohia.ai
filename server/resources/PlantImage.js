f956840e329a6f94b6e0d6f4c4e9701517f36791
Marifel Barbasa <mrbarbasa@gmail.com>
2018-11-02 21:15:45 -1000

    Create the PlantImage PUT route

---
+++ server/resources/PlantImage.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const knex = require('../knex')();
+
+const bookshelf = require('bookshelf')(knex);
+bookshelf.plugin('pagination');
+
+const PlantImage = bookshelf.Model.extend({
+  tableName: 'plant_images',
+  plant() {
+    return this.belongsTo(Plant, 'plant_id');
+  },
+});
+const Plant = bookshelf.Model.extend({
+  tableName: 'plants',
+  plantImages() {
+    return this.hasMany(PlantImage);
+  },
+});
+
+router.route('/plant-image/:id')
+  .put(function(req, res) {
+    // Get URL parameter
+    const { id } = req.params;
+
+    // Get plant image properties
+    const { plant_id, identified } = req.body;
+
+    // Update plant image
+    return PlantImage
+      .where('id', id)
+      .fetch()
+      .then((model) => {
+        model
+          .save({
+            plant_id,
+            identified,
+          })
+          .then((plantImage) => {
+            return res.json({ plantImage: plantImage.serialize() });
+          });
+      });
+  });
+
+module.exports = router;
