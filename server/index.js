35be02605c92576f1b2e8455a5118615b1378caf
Marifel Barbasa <mrbarbasa@gmail.com>
2018-11-02 21:56:07 -1000

    Enable CORS on the server

--- server/index.js
+++ server/index.js
@@ -71,6 +71,14 @@ app.use(bodyParser.urlencoded({ // Form request data
   parameterLimit: PARAM_LIMIT,
 })); 
 
+// Required for browser requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+
 // Routes
 app.use('/api', resources);
 
