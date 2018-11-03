b36d76922d54cad51ac8822e29299b92910b491a
Marifel Barbasa <mrbarbasa@gmail.com>
2018-11-02 18:58:42 -1000

    Copy code from test_model_2.js to model.js

--- server/model.js
+++ server/model.js
@@ -2,6 +2,8 @@
   Based on code from:
     http://jamesthom.as/blog/2018/08/07/
     machine-learning-in-node-dot-js-with-tensorflow-dot-js/
+  With code modified from:
+    https://github.com/tensorflow/tfjs-examples/tree/master/mobilenet
 */
 
 const fs = require('fs');
@@ -14,63 +16,94 @@ const tf = require('@tensorflow/tfjs');
 // Load the Node CPU bindings
 require('@tensorflow/tfjs-node');
 
-// Load the MobileNet pretrained model
-const mobilenet = require('@tensorflow-models/mobilenet');
+// Load dictionary mapping plant_ids to plant_names
+const PLANTNET_CLASSES = require('./models/mobilenetv1-1.00/plantnet_classes');
 
-// Load models from the filesystem
-const modelPath = path.resolve(__dirname, 'models', 'mobilenet', 'model.json');
-// MobileNet model has 3 color channels
+// Set parameters
+const TOPK = 10;
+const IMAGE_SIZE = 224;
 const NUMBER_OF_CHANNELS = 3;
 
+// Load model from the file system
+const MODEL_PATH = 'file://' + path.resolve(__dirname, 'models', 'mobilenetv1-1.00', 'model.json'); 
+
+// Return a `Uint8Array` with 4 channel values (RGBA) for each pixel
+const decodeBuffer = (buffer) => {
+  return jpeg.decode(buffer, true);
+};
+
+// Read image
 const readImage = (imagePath) => {
   const buffer = fs.readFileSync(imagePath);
   return decodeBuffer(buffer);
 };
 
-const decodeBuffer = (buffer) => {
-  // Return a `Uint8Array` with 4 channel values (RGBA) for each pixel
-  return jpeg.decode(buffer, true);
-};
-
 // Convert the 4-channel array into a 3-channel version
-const convertImageToByteArray = (image, numChannels) => {
+const convertImageToByteArray = (image) => {
   const pixels = image.data;
   const numPixels = image.width * image.height;
-  const values = new Int32Array(numPixels * numChannels);
+  const values = new Int32Array(numPixels * NUMBER_OF_CHANNELS);
 
-  for (let i = 0; i < numPixels; i++) {
-    for (let channel = 0; channel < numChannels; ++channel) {
-      values[i * numChannels + channel] = pixels[i * 4 + channel];
+  for (let i = 0; i < numPixels; i++) {    
+    for (let channel = 0; channel < NUMBER_OF_CHANNELS; ++channel) {  
+      values[i * NUMBER_OF_CHANNELS + channel] = pixels[i * 4 + channel];
     }
   }
   return values;
 };
 
-const convertImageToInput = (image, numChannels) => {
-  const values = convertImageToByteArray(image, numChannels);
-  const outShape = [image.height, image.width, numChannels];
-  return tf.tensor3d(values, outShape, 'int32');
+// Convert to 4D tensor and resize to 1 x h x w x c
+const convertImageToInput = (image) => {
+  const values = convertImageToByteArray(image);
+  const shape = [1, image.height, image.width, NUMBER_OF_CHANNELS];
+  let input = tf.tensor4d(values, shape, 'float32');
+  input = tf.image.resizeBilinear(input, [224, 224]);
+  input = input.mul(tf.scalar(1/255.));
+  input = tf.scalar(-1).add(input);
+  return input
 };
 
-const loadModel = async (modelPath) => {
-  // Manually create the MobileNet class
-  const model = new mobilenet.MobileNet(1, 1);
-  // Overwrite the HTTP address of the model with a local file system path
-  model.path = `file://${modelPath}`;
-  await model.load();
-  return model;
+// Compute top K probabilities and corresponding classes
+const getTopK = async (probabilities) => {
+  // Get sorted (descending) probabilities and indexes
+  const probsAndIndexs = [];
+  for (let i = 0; i < probabilities.length; i++) {
+    probsAndIndexs.push({ prob: probabilities[i], index: i });
+  }
+  probsAndIndexs.sort((a, b) => {
+    return b.prob - a.prob;
+  });
+
+  // Get top K locations and labels
+  const topkProbs  = new Float32Array(TOPK);
+  const topkIndexs = new Int32Array(TOPK);
+  for (let i = 0; i < TOPK; i++) {
+    topkProbs[i]  = probsAndIndexs[i].prob;
+    topkIndexs[i] = probsAndIndexs[i].index;
+  }
+
+  const topClassesAndProbs = [];
+  for (let i = 0; i < topkIndexs.length; i++) {
+    topClassesAndProbs.push({
+      className: PLANTNET_CLASSES[topkIndexs[i]],
+      probability: topkProbs[i]
+    })
+  }
+  return topClassesAndProbs;
 };
 
-const detectPlant = async (modelPath, imageBuffer) => {
+// Put it all together to detect the plant
+const detectPlant = async (imageBuffer) => {
   const image = decodeBuffer(imageBuffer);
-  const input = convertImageToInput(image, NUMBER_OF_CHANNELS);
+  const input = convertImageToInput(image);
 
-  const model = await loadModel(modelPath);
-  return await model.classify(input);
+  const model = await tf.loadModel(MODEL_PATH);
+  const probabilities = model.predict(input).dataSync();
+  return getTopK(probabilities);
 };
 
 module.exports = {
   detectPlant(imageBuffer) {
-    return detectPlant(modelPath, imageBuffer);
+    return detectPlant(imageBuffer);
   },
 };
