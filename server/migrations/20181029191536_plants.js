f9f4593b66339f05022cbc2e596fb49e41d00b06
Marifel Barbasa <mrbarbasa@gmail.com>
2018-11-04 12:40:54 -1000

    Change the endangerment level values in the migration

--- server/migrations/20181029191536_plants.js
+++ server/migrations/20181029191536_plants.js
@@ -19,10 +19,11 @@ exports.up = function(knex, Promise) {
     table.text('uses');
     table.enu('endangered',
     [
-      'Critically endangered',
       'Endangered',
-      'Vulnerable',
-      'Not endangered',
+      'Threatened',
+      'Candidate for listing',
+      'Species of concern',
+      'Not listed by state',
     ]);
 
     table.timestamp('created_at').defaultTo(knex.fn.now());
