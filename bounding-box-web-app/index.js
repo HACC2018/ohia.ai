02110cc86fc74dc984f9a6d037361ed9e550dfa1
sorapk <44356222+sorapk@users.noreply.github.com>
2018-11-07 22:02:47 -1000

    experimentation with real-time bound boxing

---
+++ bounding-box-web-app/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'pages'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+console.log("hello world")
+
